066a5cc31cb457c6a1a1ba6e33cff7563826a57c
TheBadAngel <144236013+TheBadAngel@users.noreply.github.com>
2024-04-11 07:41:42 +0200

    Add files via upload

---
+++ Hollo World - First Program/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Hollo_World___First_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Guys!");
+            Console.WriteLine("My Name is TheBadAngel");
+            Console.WriteLine("It is nice to meet you :)");
+            Console.WriteLine("I am larning to program\n");
+                        
+            Console.WriteLine("Make a program that will let Telim right who got bread, and get a conformation.\n");
+
+            Console.WriteLine("The bread is ready.\nWho is it for?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Noted: {name} got bread.\n");
+
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            Console.ReadLine();
+
+
+        }
+    }
+}
