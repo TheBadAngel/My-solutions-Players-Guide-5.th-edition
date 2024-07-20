8c82fa28fcb2a2d743d36da100acad405837de95
TheBadAngel <elisabethelida@hotmail.com>
2024-07-20 10:32:30 +0200

    new

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Hollo_World___First_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Level 1 and 2 is introduction and intall of VS.
+
+            //Level 3 challenge 1
+            Console.WriteLine("Hello, Guys!");
+            Console.WriteLine("My Name is TheBadAngel");
+            Console.WriteLine("It is nice to meet you :)");
+            Console.WriteLine("I am learning to program\n");
+            
+
+
+            //Level 3 challenge 2
+            Console.WriteLine("Make a program that will let Telim right who got bread, and get a conformation.\n");
+
+            Console.WriteLine("The bread is ready.\nWho is it for?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Noted: {name} got bread.\n");
+
+
+
+            //Level 4 "The name thing 3000"
+            Console.WriteLine("What kind of thing are we talking aboudt?");
+            // String a stores the name of the ting.
+            string a = Console.ReadLine(); 
+            Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+            //String b stores the descripsion of the thing
+            string b = Console.ReadLine();
+            /*String c and d stores "of doom" text.*/
+            string c = "of Domm";
+            //String d stores the nummber
+            string d = "3000";
+            Console.WriteLine($"The {b} {a} {c} {d}!");
+            //Removed the extra "of" to correct the "of of" problem
+            //I would rename the variable names so the descripe the information they are storing.
+
+
+
+
+            //Level 5
+
+            //Knowledge Check
+            //1. Name the 3 things all variables have.
+            //   Type, Name, Data.
+            //2. True/False. Variables must always be declared before being used.
+            //   Yes, or at the same time
+            //3. Can you redeclare a variable?
+            //   No, but you can store new informatin in a variable thats has been used before.
+            //4. Witch of the following are legal C# variables names? answer, 1stValue, value1, $message, delete-me, delete_me, PI
+            //   answer, value1, delete-me, PI
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
